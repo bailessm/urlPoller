b0994e106e3d92c2de44ab3b80195315395d9c4f
Bailess <bailess@a45e60ecbfb5.ant.amazon.com>
2019-06-07 11:21:55 -0400

    refactored deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+
+### capture input variables
 for ARGUMENT in "$@"
 do
 
@@ -14,10 +16,11 @@ do
 
 done
 
-stackName=${stackName:-'instanceScheduler'}
+##fill in required default variables
+stackName=${stackName:-'PollerStack'}
 region=${region:-'us-east-1'}
 
-
+###set the default profile to be used connecting to aws if you passed one.
 if [ ! -z $defaultProfile ] 
 then 
     export AWS_DEFAULT_PROFILE=$defaultProfile
@@ -31,18 +34,25 @@ aws cloudformation wait stack-create-complete --stack-name $stackName-Bucket --r
 
 ##pull the s3Bucket Output from the bucket
 s3BucketArn=$(aws cloudformation describe-stacks --stack-name $stackName-Bucket \
-    --query 'Stacks[0].Outputs[?OutputKey==`s3Bucket`].OutputValue[]' --region $region)
+    --query 'Stacks[0].Outputs[?OutputKey==`s3Bucket`].OutputValue[]' --region $region --output text)
 
 ##Pull the S3 bucket name from the ARN
-s3BucketArn="${s3BucketArn//[}"
-s3BucketArn="${s3BucketArn//]}"
-s3BucketArn=${s3BucketArn//$'\n'/}
-s3BucketArn=${s3BucketArn//$'"'/}
-s3BucketArn=${s3BucketArn//$' '/}
 bucketName=${s3BucketArn//$'arn:aws:s3:::'/}
 
-##upload the lambda code to the code bucket
-aws s3 cp . s3://$bucketName/ --recursive --exclude "*" --include "*.zip"
+##zip and upload the lambda code to the code bucket
+for i in $(find . -name '*py')
+do  
+    file=$(echo $i | cut -d'/' -f3)
+    echo $file
+    zipFile=${file//$'.py'/}
+    echo $zipFile
+    echo $i
+    zip $zipFile-latest.zip $i -j
+done
+
+aws s3 cp . s3://$bucketName/ --recursive --exclude "*" --include "*-latest.zip"
+
+find . -name '*-latest.zip' | xargs rm
 
 ##Create URL Poller stack
 aws cloudformation create-stack --stack-name $stackName \
@@ -53,18 +63,19 @@ aws cloudformation create-stack --stack-name $stackName \
 #Wait for the stack to be created
 echo "Please wait for the stack to be created..."
 aws cloudformation wait stack-create-complete --stack-name $stackName --region $region
+
 echo "The stack application is ready, loading test URLs..."
 
+#get the arn for the lambda function to inupt test records
 lambdaArn=$(aws cloudformation describe-stacks --stack-name $stackName \
-    --query 'Stacks[0].Outputs[?OutputKey==`testRecordsFunction`].OutputValue[]' --region $region)
+    --query 'Stacks[0].Outputs[?OutputKey==`testRecordsFunction`].OutputValue[]' --region $region \
+    --output text)
 
-lambdaArn="${lambdaArn//[}"
-lambdaArn="${lambdaArn//]}"
-lambdaArn=${lambdaArn//'"'}
-
+#invoke function to input test records.
 aws lambda invoke --function-name $lambdaArn --region $region outputfile.txt
 rm outputfile.txt
 
+## revert to the original default profile if you changed it for this script
 if [ ! -z $defaultProfile ] 
 then 
     unset AWS_DEFAULT_PROFILE
